53ed6bcf0c0470c375554aedb669011fc7bd3fb9
Louis Galloux <lgalloux@c2r12p2.42lehavre.fr>
2025-05-07 14:35:14 +0200

    remake ex00 add energypoint-- and hitpoint++

--- cpp03/ex00/include/ClapTrap.hpp
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -5,19 +5,28 @@
 
 class ClapTrap
 {
-  private:
+  protected:
     std::string  mName;
     int          mHitPoints;
     int          mEnergyPoints;
     int          mAttackDamage;
   public:
-    ClapTrap(const std::string& name = "");
+    ClapTrap(const std::string& name = "", int hPoints = 10
+      ,int ePoints = 10, int aDamgage = 0);
     ClapTrap(const ClapTrap& clapTrap);
     ClapTrap&  operator=(const ClapTrap& clapTrap);
     ~ClapTrap();
-    void  attack(const std::string& target);
-    void  takeDamage(unsigned int amount);
-    void  beRepaired(unsigned int amount);
+    void                attack(const std::string& target);
+    void                takeDamage(unsigned int amount);
+    void                beRepaired(unsigned int amount);
+    const std::string&  getName(void) const;
+    int                 getHpoints(void) const;
+    int                 getEpoints(void) const;
+    int                 getAdamage(void) const;
+    void                setName(const std::string& name);
+    void                setHpoints(const int hPoints);
+    void                setEpoints(const int hePoints);
+    void                setAdamage(const int aDamage);
 };
 
 #endif
--- cpp03/ex00/src/ClapTrap.cpp
+++ cpp03/ex00/src/ClapTrap.cpp
@@ -1,13 +1,13 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
-ClapTrap::ClapTrap(const std::string& name)
+ClapTrap::ClapTrap(const std::string& name, int hPoints, int ePoints, int aDamage)
   :mName(name)
-  ,mHitPoints(10)
-  ,mEnergyPoints(10)
-  ,mAttackDamage(0)
+  ,mHitPoints(hPoints)
+  ,mEnergyPoints(ePoints)
+  ,mAttackDamage(aDamage)
 {
-  std::cout << "(ClapTrap) construcor called\n";
+  std::cout << "(ClapTrap) constructor called\n";
 }
 
 ClapTrap::ClapTrap(const ClapTrap& clapTrap)
@@ -21,7 +21,7 @@ ClapTrap::ClapTrap(const ClapTrap& clapTrap)
 
 ClapTrap&  ClapTrap::operator=(const ClapTrap& clapTrap)
 {
-  std::cout << "(ClapTrap) copy assignement operator called \n";
+  std::cout << "(ClapTrap) copy assignement operator called\n";
   if (this == &clapTrap)
     return (*this);
   mName = clapTrap.mName;
@@ -33,25 +33,72 @@ ClapTrap&  ClapTrap::operator=(const ClapTrap& clapTrap)
 
 ClapTrap::~ClapTrap()
 {
-  std::cout << "ClapTrap destroyed\n";
+  std::cout << "(ClapTrap) destructor called\n";
 }
 
 void  ClapTrap::attack(const std::string& target)
 {
   if (mEnergyPoints && mHitPoints)
-    std::cout << "ClapTrap " << mName << " attacks " <<
+  {
+    std::cout << "(ClapTrap) " << mName << " attacks " <<
       target << " causing " << mAttackDamage << " points of damage !" << '\n';
+    setEpoints(getEpoints()-1);
+  }
 }
 
 void  ClapTrap::takeDamage(unsigned int amount)
 {
-  std::cout << "ClapTrap " << mName << " has taken " <<
+  setHpoints(getHpoints() - amount);
+  std::cout << mName << " has taken " <<
     amount << " points of damage!" << '\n';
 }
 
 void  ClapTrap::beRepaired(unsigned int amount)
 {
   if (mEnergyPoints && mHitPoints)
-    std::cout << "ClapTrap " << mName << " is being repaired by " <<
+  {
+    std::cout << mName << " is being repaired by " <<
       amount << " points !" << '\n';
+    setEpoints(getEpoints()-1);
+  }
+}
+
+const  std::string& ClapTrap::getName(void) const
+{
+  return (mName);
+}
+
+int  ClapTrap::getHpoints(void) const
+{
+  return (mHitPoints);
+}
+
+int  ClapTrap::getEpoints(void) const
+{
+  return (mEnergyPoints);
+}
+
+int  ClapTrap::getAdamage(void) const
+{
+  return (mAttackDamage);
+}
+
+void  ClapTrap::setName(const std::string& name)
+{
+  mName = name;
+}
+
+void  ClapTrap::setHpoints(const int hPoints)
+{
+  mHitPoints = hPoints;
+}
+
+void  ClapTrap::setEpoints(const int ePoints)
+{
+  mEnergyPoints = ePoints;
+}
+
+void  ClapTrap::setAdamage(const int aDamage)
+{
+  mAttackDamage = aDamage;
 }
