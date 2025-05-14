92700862d590000c0f8285325f3a936a46155842
thryndir <neon.galgamergal@gmail.com>
2025-05-14 16:10:49 +0200

    add ex01

---
+++ cpp08/ex01/include/easyfind.hpp
@@ -0,0 +1,11 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <cstddef>
+
+template<typename T>
+size_t easyfind(T container, int toFind);
+
+#include "easyfind.tpp"
+
+#endif
---
+++ cpp08/ex01/src/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include "easyfind.hpp"
+
+int main( void )
+{
+  try
+  {
+    std::vector<int> vector;
+    vector.reserve(5);
+    for (int i = 1; i <= 5; ++i)
+      vector.push_back(i);
+    std::cout << "vector " << easyfind(vector, 32) << " has been found\n";
+    std::list<int> list;
+    list.push_back(8);
+    list.push_front(2);
+    list.push_front(3);
+    list.push_front(4);
+    list.push_front(1);
+    std::cout << "list "<< easyfind(list, 32) << " has been found\n";
+  }
+  catch (const std::exception& e)
+  {
+    std::cout << e.what();
+  }
+  return 0;
+}
